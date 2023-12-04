97a689ec28b4190f3b35bd5f66b1b8dbd3e94856
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2023-12-04 14:31:56 +0000

    mejoras

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+  
+    sidebar.classList.toggle('active');
+    content.classList.toggle('active');
+  }
+  
