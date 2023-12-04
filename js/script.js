11a8af36f52dccd85a602d08bfcd499487b58636
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2023-12-04 19:46:33 +0000

    agregue el mapa

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.width === "250px") {
-      sidebar.style.width = "0";
-  } else {
-      sidebar.style.width = "250px";
+    const sidebar = document.getElementById("sidebar");
+    const content = document.getElementById("content");
+    if (sidebar.style.width === "0px") {
+        sidebar.style.width = "250px";
+        content.style.marginLeft = "250px";
+    } else {
+        sidebar.style.width = "0px";
+        content.style.marginLeft = "0px";
+    }
   }
-}
 window.onload = function() {
   var submenus = document.getElementsByClassName('submenu');
   for (var i = 0; i < submenus.length; i++) {
@@ -31,3 +34,47 @@ function toggleSidebar() {
       content.style.marginLeft = "250px";
   }
 }
+
+
+
+//mapa 
+//toggle button
+$('.js-toggle').on('click', function(){
+	$('.map-base').toggleClass('active');
+});
+
+
+//initialization of speech recognition starts here
+const artyom = new Artyom();
+var commands = [
+ {
+	 indexes:["up to no good"],
+	 action:function() {
+		 $('.map-base').addClass('active');
+	 }
+ }, {
+	 indexes:["mischief managed"],
+	 action:function() {
+		 $('.map-base').removeClass('active');
+	 }
+ }
+]
+
+artyom.addCommands(commands);
+
+function startContinuousArtyom(){
+    artyom.fatality();
+
+    setTimeout(function(){
+         artyom.initialize({
+            lang:"en-GB",
+            continuous:true,
+            listen:true, 
+            speed:1
+        }).then(function(){
+            console.log("Ready to work !");
+        });
+    },250);
+}
+
+startContinuousArtyom();
