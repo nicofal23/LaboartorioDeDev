c10561b2769c55505c7a2489103ab77b4f099b15
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2023-12-04 16:42:50 +0000

    pagina dev

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const content = document.getElementById('content');
-  
-    sidebar.classList.toggle('active');
-    content.classList.toggle('active');
+  const sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "250px") {
+      sidebar.style.width = "0";
+  } else {
+      sidebar.style.width = "250px";
   }
-  
+}
+window.onload = function() {
+  var submenus = document.getElementsByClassName('submenu');
+  for (var i = 0; i < submenus.length; i++) {
+      submenus[i].addEventListener('click', function() {
+          var sublist = this.getElementsByClassName('sublist')[0];
+          if (sublist.style.display === "none") {
+              sublist.style.display = "block";
+          } else {
+              sublist.style.display = "none";
+          }
+      });
+  }
+}
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const content = document.getElementById("content");
+  if (sidebar.style.width === "250px") {
+      sidebar.style.width = "0";
+      content.style.marginLeft = "0";
+  } else {
+      sidebar.style.width = "250px";
+      content.style.marginLeft = "250px";
+  }
+}
